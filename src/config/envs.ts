e2c9a1004fdc1a886122f6b8aa8177049cafef31
Cesar Espinosa <arionespinosam@gmail.com>
2024-04-25 13:31:00 -0600

    Configurando Prisma-SQLite  completado y funcional

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,10 +3,12 @@ import * as joi from "joi";
 
 interface EnvVars {
     PORT: number;
+    DATABASE_URL: string;
 }
 
 const envsSchema = joi.object({
-    PORT: joi.number().required()
+    PORT: joi.number().required(),
+    DATABASE_URL: joi.string().required()
 })
 .unknown(true);
 
@@ -20,5 +22,6 @@ const envVars: EnvVars = value;
 
 export const envs = {
     port: envVars.PORT,
+    databaseUrl: envVars.DATABASE_URL,
 }
 
